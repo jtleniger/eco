64399de0340642c0e193923a63acaa6b067d58a5
Justin Leniger <jtleniger@gmail.com>
2022-08-22 11:02:33 -0600

    migrated eats to behavior

--- src/creature.ts
+++ src/creature.ts
@@ -1,11 +1,11 @@
 import p5 from 'p5'
-import Food from './food'
 import Sprite from './sprite'
 import State from './state'
 import Rests from './traits/rests'
 import Behavior from './traits/behavior'
 import World from './world'
 import DNA from './genes/dna'
+import Eats from './traits/eats'
 
 class Creature extends Sprite {
   private static readonly FOOD_SEARCH_RADIUS = 256
@@ -23,21 +23,19 @@ class Creature extends Sprite {
   }
 
   world: World
-  nearbyFood: Food | null
   nearbyMate: Creature | null
-  fed: number
   direction: p5.Vector
-  hungerTimer: number | null
   unavailableTimer: number | null
   state: Set<number>
   newState: Set<State>
   dna: DNA = DNA.Default()
 
-  private readonly traits: Behavior[] = [new Rests(this.dna.rest, this.onRestEnd.bind(this))]
+  private readonly traits: Behavior[]
 
   constructor(sketch: p5, world: World, pos: p5.Vector) {
     super(sketch, pos)
     this.world = world
+    this.traits = [new Rests(this.dna.rest), new Eats(this.dna.eat, this.pos, this.world.food)]
     this.reset()
   }
 
@@ -45,11 +43,7 @@ class Creature extends Sprite {
     this.state = new Set()
     this.newState = new Set()
     this.unavailableTimer = null
-    this.nearbyFood = null
     this.nearbyMate = null
-    this.fed = 5
-    this.setRandomDirection()
-    this.hungerTimer = null
     this.unavailableTimer = null
   }
 
@@ -58,44 +52,25 @@ class Creature extends Sprite {
       t.update(this.newState)
     }
     // this.mate()
-    // this.hunt()
-    // this.hunger()
-    this.health()
+    // this.health()
     this.move()
   }
 
-  draw(): void {
-    if (this.state.has(Creature.STATES.Mating)) {
-      const grey = this.sketch.map(this.fed, 0, 10, 0, 192)
-      this.sketch.tint(255, grey, grey)
-    } else {
-      this.sketch.tint(this.sketch.map(this.fed, 0, 10, 0, 255))
-    }
+  // draw(): void {
+  //   if (this.state.has(Creature.STATES.Mating)) {
+  //     const grey = this.sketch.map(this.fed, 0, 10, 0, 192)
+  //     this.sketch.tint(255, grey, grey)
+  //   } else {
+  //     this.sketch.tint(this.sketch.map(this.fed, 0, 10, 0, 255))
+  //   }
 
-    super.draw()
-    this.sketch.noTint()
-  }
+  //   super.draw()
+  //   this.sketch.noTint()
+  // }
 
   /*
    * STATE METHODS
    */
-
-  hunt(): void {
-    if (this.state.has(Creature.STATES.Hunting) && this.nearbyFood !== null) {
-      this.tryEat()
-      return
-    }
-
-    const maybeFood = this.nearestFood()
-
-    if (maybeFood === null) {
-      return
-    }
-
-    this.state.add(Creature.STATES.Hunting)
-    this.nearbyFood = maybeFood
-    this.setFoodDirection()
-  }
 
   mate(): void {
     if (!this.state.has(Creature.STATES.Available)) {
@@ -115,37 +90,34 @@ class Creature extends Sprite {
 
     this.state.add(Creature.STATES.Mating)
     this.nearbyMate = maybeMate
-    this.endForage()
-  }
-
-  hunger(): void {
-    if (this.hungerTimer !== null) {
-      return
-    }
-
-    this.hungerTimer = window.setTimeout(() => {
-      this.fed = this.sketch.constrain(this.fed - 1, 0, 10)
-      this.hungerTimer = null
-    }, 15000)
   }
 
   health(): void {
-    if (this.fed === 0) {
-      this.world.kill(this)
-    }
-
-    if (this.fed < 7) {
-      this.state.delete(Creature.STATES.Mating)
-      this.state.delete(Creature.STATES.Available)
-    } else if (this.unavailableTimer === null) {
-      this.state.add(Creature.STATES.Available)
-    }
+    // if (this.fed === 0) {
+    //   this.world.kill(this)
+    // }
+    // if (this.fed < 7) {
+    //   this.state.delete(Creature.STATES.Mating)
+    //   this.state.delete(Creature.STATES.Available)
+    // } else if (this.unavailableTimer === null) {
+    //   this.state.add(Creature.STATES.Available)
+    // }
   }
 
   move(): void {
-    if (this.newState.has(State.Resting)) {
+    const dirs = this.traits.map((t) => t.direction(this.newState)).filter((d) => d !== null)
+
+    if (dirs.length > 1) {
+      throw Error('received multiple candidate directions')
+    }
+
+    const dir = dirs[0]
+
+    if (dir === null || dir === undefined) {
       return
     }
+
+    this.direction = dir
 
     if (
       this.pos.x >= this.sketch.width ||
@@ -165,29 +137,6 @@ class Creature extends Sprite {
   /*
    * HELPER METHODS
    */
-  nearestFood(): Food | null {
-    let minDistance = Infinity
-    let food = null
-
-    this.world.food.forEach((f) => {
-      if (f.eaten) {
-        return
-      }
-
-      const distance = f.pos.dist(this.pos)
-
-      if (distance > Creature.FOOD_SEARCH_RADIUS) {
-        return
-      }
-
-      if (distance < minDistance) {
-        minDistance = distance
-        food = f
-      }
-    })
-
-    return food
-  }
 
   nearestMate(): Creature | null {
     let minDistance = Infinity
@@ -217,44 +166,12 @@ class Creature extends Sprite {
     return creature
   }
 
-  setRandomDirection(): void {
-    this.direction = p5.Vector.random2D()
-  }
-
-  setFoodDirection(): void {
-    if (this.nearbyFood === null) {
-      return
-    }
-
-    this.direction = this.nearbyFood.pos.copy().sub(this.pos).normalize()
-  }
-
   setMateDirection(): void {
     if (this.nearbyMate === null) {
       return
     }
 
     this.direction = this.nearbyMate.pos.copy().sub(this.pos).normalize()
-  }
-
-  tryEat(): void {
-    if (this.nearbyFood === null) {
-      return
-    }
-
-    if (this.nearbyFood.eaten) {
-      this.endForage()
-      return
-    }
-
-    const dist = this.pos.dist(this.nearbyFood.pos)
-
-    if (dist < this.img.width) {
-      this.nearbyFood.eaten = true
-      this.fed = this.sketch.constrain(this.fed + 1, 0, 10)
-      this.clearHungerTimer()
-      this.endForage()
-    }
   }
 
   tryMate(): void {
@@ -297,26 +214,6 @@ class Creature extends Sprite {
   endMate(): void {
     this.nearbyMate = null
     this.state.delete(Creature.STATES.Mating)
-  }
-
-  endForage(): void {
-    this.nearbyFood = null
-    this.state.delete(Creature.STATES.Hunting)
-  }
-
-  clearHungerTimer(): void {
-    if (this.hungerTimer === null) {
-      return
-    }
-
-    window.clearTimeout(this.hungerTimer)
-    this.hungerTimer = null
-  }
-
-  onRestEnd(): void {
-    if (!this.state.has(Creature.STATES.Hunting) && !this.state.has(Creature.STATES.Mating)) {
-      this.setRandomDirection()
-    }
   }
 }
 
